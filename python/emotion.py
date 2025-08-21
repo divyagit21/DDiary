ea91d5869604d893b1eca7a1dbe499d911d2e38d
divyagit21 <mittasaidivya21@gmail.com>
2025-08-22 01:55:19 +0530

    first commit

--- python/emotion.py
+++ python/emotion.py
@@ -3,7 +3,7 @@ from transformers import pipeline
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app,supports_credentials=True, origins=["http://localhost:3000"]) 
+CORS(app) 
 
 emotion_classifier = pipeline("text-classification", model="ayoubkirouane/BERT-Emotions-Classifier", return_all_scores=True)
 
