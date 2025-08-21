0d2a3973d51937d638d3c7e7c678c256dd152ac5
divyagit21 <mittasaidivya21@gmail.com>
2025-08-22 01:36:22 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,7 @@ mongoose.connect(process.env.MONGO_CONN)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true, 
-}));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 
