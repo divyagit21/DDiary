ede8dc00207714061f680121c6587c510d06ef02
divyagit21 <mittasaidivya21@gmail.com>
2025-08-26 21:52:53 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const moodTracker = require('./routes/moodTracker');
 const userAuth = require('./routes/userAuth');
 const journal = require('./routes/journal');
 const task = require('./routes/tasks');
-const env=require('dotenv');
+const env = require('dotenv');
 const cookieParser = require('cookie-parser');
 env.config()
 const app = express();
@@ -15,15 +15,15 @@ mongoose.connect(process.env.MONGO_CONN)
   .catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(cors({
-  origin: 'https://d-diary-fcna.vercel.app', 
-  credentials: true, 
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser())
 
 app.use('/api/moodTracker', moodTracker);
 app.use('/api/user', userAuth);
-app.use('/api/journal',journal);
-app.use('/api/tasks',task);
+app.use('/api/journal', journal);
+app.use('/api/tasks', task);
 
 app.listen(5000, () => console.log('node server running on port 5000'));
